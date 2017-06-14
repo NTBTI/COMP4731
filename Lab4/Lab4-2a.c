8ad77cf6b9af47ce8be838b36ff299cfeded3de9
Toms Macbook <tom@ntbti.com>
2017-06-13 20:22:56 -0700

    Albert doesn't like printf() so switched to write() and a read buffer. Lab4 complete.

--- Lab4/Lab4-2a.c
+++ Lab4/Lab4-2a.c
@@ -11,6 +11,7 @@ void *readFIFO(void *arg)
     //cast to a string again
     char *fifoPath = (char *)arg;
     char buffer[BUFSIZE];
+    ssize_t bytesRead; //set this instead so we don't use printf()
 
     fp = open(fifoPath, O_RDONLY);
     //check the file opened
@@ -21,12 +22,10 @@ void *readFIFO(void *arg)
         pthread_exit(&fp);
     }
 
-    while (1)
+    while ((bytesRead = read(fp, buffer, BUFSIZE)) > 0)
     {
-        if (read(fp, buffer, BUFSIZE))
-        {
-            printf("%s", buffer);
-        }
+        //printf("%s", buffer); //print continues to read, but it may or may NOT contain a null term at the end, so use write() instead
+        write(STDOUT_FILENO, buffer, bytesRead);
     }
 
     pthread_exit(0);
