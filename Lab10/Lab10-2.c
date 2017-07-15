5204d8f83fa68f1fad4b19f74d5381db98cae134
Toms Macbook <tom@ntbti.com>
2017-07-14 19:00:56 -0700

    Lab10 complete

--- Lab10/Lab10-2.c
+++ Lab10/Lab10-2.c
@@ -5,21 +5,36 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <string.h>
 
 #define DEBUG 1
+
+#define MAXBUF 1024
 
 //so there goes an hour...*nix uses MAP_ANONYMOUS and OS X uses MAP_ANON
 #ifdef __APPLE__
 #define MAP_ANONYMOUS MAP_ANON
 #endif
 
-int main(void)
+int main(int argc, char **argv)
 {
-    int *addr;
+    //make sure we have something
+    if (argc < 2)
+    {
+        printf("Need to give something to store\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //int *addr;
+    char *addr;
     pid_t pid;
+    size_t textLen;
+
+    const char *textToStore = argv[1];
+    textLen = strlen(textToStore) + 1; //Stupid NULL terminator...
 
     //set anon-access
-    addr = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    addr = mmap(0, textLen, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if (addr == MAP_FAILED)
     {
         perror("Mapping failed\n");
@@ -35,13 +50,17 @@ int main(void)
         printf("Error forking\n");
         return EXIT_FAILURE;
     case 0: //Child
-        *addr = 90210;
-        if (DEBUG)
+        for (size_t i = 0; i < textLen; i++)
         {
-            printf("In the child process. Writing the value %d\n", *addr);
+            addr[i] = textToStore[i];
         }
 
-        if (munmap(addr, sizeof(int)) < 0)
+        if (DEBUG)
+        {
+            printf("In the child process. Writing the value %s\n", addr);
+        }
+
+        if (munmap(addr, textLen) < 0)
         {
             //failed to write to shared mem
             perror("Error writing to shared mem\n");
@@ -49,9 +68,9 @@ int main(void)
         }
     default:        //parent
         wait(&pid); //wait for the child to die.
-        printf("Value from child is %d\n", *addr);
+        printf("Value from child is '%s'\n", addr);
 
-        if (munmap(addr, sizeof(int)) == -1)
+        if (munmap(addr, textLen) == -1)
         {
             perror("Could not unmap\n");
             return EXIT_FAILURE;
